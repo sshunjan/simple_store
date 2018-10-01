56a6f90104f5b7baed9e29267e3518a8777f6b51
Simranjeet Singh <sshunjan97@gmail.com>
2018-10-01 13:22:11 -0500

    Added the validations and remove the comma  errors in the db file.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+
+  validates :title, persistence: true
+  validates :description, persistence: true
+  validates :stock_quantity, persistence: true
+
 end
--- db/migrate/20181001181240_create_products.rb
+++ db/migrate/20181001181240_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string, :title
-      t.text, :decription
-      t.decimal, :price
+      t.string :title
+      t.text :decription
+      t.decimal :price
       t.integer :stock_quantity
 
       t.timestamps
