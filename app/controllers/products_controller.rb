0af76a1d4f814929429fc84370c09d45dde52747
Simranjeet Singh <sshunjan97@gmail.com>
2018-10-02 08:25:41 -0500

    Modified the view and contoller to add some figures.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
   def index
     @all_products = Product.all()
+    @total_number_of_products = Product.count()
+
+    @total_price_amount = 0.00;
+    @total_quantity = 0;
+    @all_products.each do |p|
+      @total_price_amount += p.price.to_f
+      @total_quantity += p.stock_quantity
+    end
+
+    @average_price = @total_price_amount/@total_number_of_products
+
   end
 
   def show
